2a8563ef5ad846c17fe5130938e621ca434c2939
Myriam <mireilledenis93@gmail.com>
2025-11-06 14:29:03 +0100

    liste film filtre genre

--- read/read_data.py
+++ read/read_data.py
@@ -17,6 +17,7 @@ def recuperer_un_film_grace_au_titre():
                 break
 
 def recuperer_liste_films_grace_limite_age():
+    liste_films = []
     age_limite = input("Quel est l'âge limite ?")
     if not age_limite.isnumeric():
         raise Exception("L'âge doit être un nombre !")
@@ -28,9 +29,16 @@ def recuperer_liste_films_grace_limite_age():
                 pass
             else :
                 if int(ligne[4]) <= int(age_limite):
-                    print(f"ID : {ligne[0]}")
-                    print(f"Titre : {ligne[1]}")
-                    print(f"Année de production : {ligne[2]}")
-                    print(f"Genre : {ligne[3]}")
-                    print(f"Age conseillé : {ligne[4]}")
+                    liste_films.append(ligne)
+    print(liste_films)
             
+def recuperer_liste_films_grace_genre():
+    liste_films_genre =[]
+    genre = input("Quel est le genre ?")
+    
+    with open("data/movies.csv", mode='r', encoding='utf-8') as fichier:
+        lecteur = csv.reader(fichier)
+        for index, ligne in enumerate(lecteur):
+            if ligne[3].lower() == genre.lower():
+                liste_films_genre.append(ligne)
+    print(liste_films_genre)
